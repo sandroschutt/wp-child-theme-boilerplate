8cc6e657d336a9146b9a082fff81967805759ac0
sandroschutt <dev.sandroschutt@gmail.com>
2025-08-24 19:46:08 -0300

    Fix modules logic and improve some things

--- inc/Assets/Styles.php
+++ inc/Assets/Styles.php
@@ -16,9 +16,12 @@ class Styles
      */
     private $stylesPath;
 
+    private $fileExt;
+
     public function __construct()
     {
         $this->stylesPath = get_theme_file_uri() . "/build/css/";
+        $this->fileExt = ".min.css";
     }
 
     /**
@@ -39,17 +42,14 @@ class Styles
             wp_enqueue_style('home');
         }
 
-        $styles = \WPChild\Helpers::getFilesArray("/build/css/");
-        $fileExt = ".min.css";
+        $fileExt = $this->fileExt;
+        $styles = \WPChild\Helpers::getFilesArray("/build/css/", $fileExt);
 
         if (is_admin() || $styles === false) return;
 
-        if (count($styles['files']) <= 2) return;
         foreach ($styles['files'] as $style) :
-            if (strlen($style) >= 3 && str_contains($style, "$fileExt")) :
-                $handle = preg_replace("/$fileExt/", "", $style);
-                wp_enqueue_style($handle, $this->stylesPath . $style);
-            endif;
+            $handle = preg_replace("/$fileExt/", "", $style);
+            wp_enqueue_style($handle, $this->stylesPath . $style);
         endforeach;
     }
 
@@ -62,19 +62,16 @@ class Styles
      */
     public function enqueuePageStyles()
     {
-        $styles = \WPChild\Helpers::getFilesArray("/build/css/pages/");
-        $fileExt = ".min.css";
+        $fileExt = $this->fileExt;
+        $styles = \WPChild\Helpers::getFilesArray("/build/css/pages/", $fileExt);
 
         if (is_admin() || $styles === false) return;
 
-        if (count($styles['files']) <= 2) return;
         foreach ($styles['files'] as $style) :
-            if (strlen($style) >= 3 && str_contains($style, $fileExt)) :
-                $slug = preg_replace("/$fileExt/", "", $style);
-                $page = preg_replace("/page-/", "", $slug);
-                if (is_page($page)) :
-                    wp_enqueue_style("page-" . $page, $this->stylesPath . "pages/$style");
-                endif;
+            $slug = preg_replace("/$fileExt/", "", $style);
+            $page = preg_replace("/page-/", "", $slug);
+            if (is_page($page)) :
+                wp_enqueue_style("page-" . $page, $this->stylesPath . "pages/$style");
             endif;
         endforeach;
     }
--- inc/Helpers.php
+++ inc/Helpers.php
@@ -19,15 +19,26 @@ class Helpers
      * directory is empty or only contains system entries.
      *
      * @param string $path Relative path from the parent directory to scan.
+     * 
+     * @param string $fileExt File extension to look for. Will list all files if this is not set.
      *
      * @return array|bool Returns an array with keys 'files' and 'dir' if files exist or false if the directory is empty or contains no files.
      */
-    public static function getFilesArray(String $path): array|Bool
+    public static function getFilesArray(String $path, String $fileExt = ""): array|Bool
     {
         $dir = dirname(__DIR__) . $path;
         $readFiles = scandir($dir, SCANDIR_SORT_DESCENDING);
-        if (count($readFiles) <= 2) return false;
         $readFiles = ["files" => $readFiles, "dir" => $dir];
+
+        if ($fileExt !== "") :
+            foreach ($readFiles['files'] as $key => $file) :
+                if (strlen($file) <= 2 || !str_contains($file, "$fileExt")) :
+                    unset($readFiles['files'][$key]);
+                endif;
+            endforeach;
+        endif;
+
+        if (count($readFiles['files']) < 1) return false;
         return $readFiles;
     }
 }
